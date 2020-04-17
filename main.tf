d60503baea83823b3f62f883cbaaedd041eb2e8b
Johannes Kastl <kastl@b1-systems.de>
2020-04-17 10:37:06 +0200

    main.tf: adjust formatting the way 'terraforn fmt' says it is right...

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@
 
 data "template_file" "etc_hosts_snippet" {
   template = <<EOT
-%{ for group in var.groups_to_add ~}
-# ${ group.name }
-%{ for fip_association in group.instancesobject.floating_ip_associations.* ~}
-%{ for host in group.instancesobject.instances.* ~}
-%{ if host.id == fip_association.instance_id ~}
+%{for group in var.groups_to_add~}
+# ${group.name}
+%{for fip_association in group.instancesobject.floating_ip_associations.*~}
+%{for host in group.instancesobject.instances.*~}
+%{if host.id == fip_association.instance_id~}
 ${fip_association.floating_ip} ${host.name} ${host.name}.training.b1-systems.de
-%{ endif ~}
-%{ endfor ~}
-%{ endfor ~}
+%{endif~}
+%{endfor~}
+%{endfor~}
 
-%{ endfor ~}
+%{endfor~}
 EOT
 }
 
